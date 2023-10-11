70236a6ea5f35cbe0bb7917edf3fa4f1ef5d5460
Bayezid075 <mrbayezidhoshen@gmail.com>
2023-10-11 08:28:33 +0600

    edit added

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -3,14 +3,19 @@ import "./style.css";
 import React, { useState } from "react";
 
 export default function Student() {
+  // create state
   const [name, setName] = useState("");
   const [list, setList] = useState([
     { id: Math.random(), listName: "Bayezid Hoshen" },
   ]);
+
+  // edit state
+  const [editmode, setEditmode] = useState(false);
+  const [editableNote, setEditableNote] = useState(null);
   // create student start
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(name);
+
     if (!name) {
       return alert("Add A Name😊");
     }
@@ -22,10 +27,34 @@ export default function Student() {
   };
   // create Student end
 
+  // edit Student
+  const editHandler = (note) => {
+    setName(note.listName);
+    setEditableNote();
+    setEditmode(true);
+    setEditableNote(note);
+  };
+  const updateHandler = (e) => {
+    e.preventDefault();
+
+    const updatedArr = list.map((item) => {
+      if (item.id === editableNote.id) {
+        return {
+          ...item,
+          listName: name,
+        };
+      }
+      return item;
+    });
+    setList(updatedArr);
+    setEditmode(false);
+    setName("");
+  };
+  // edit student end
   return (
     <div className="main">
       <div className="add_student">
-        <form onSubmit={submitHandler}>
+        <form onSubmit={editmode ? updateHandler : submitHandler}>
           <input
             type="text"
             placeholder="Add Student"
@@ -33,7 +62,7 @@ export default function Student() {
             value={name}
           />
           <button className="add_button" type="submit">
-            ADD
+            {editmode ? "Update Name" : "ADD"}
           </button>
         </form>
       </div>
@@ -42,16 +71,20 @@ export default function Student() {
           <h3> Present Student</h3>
           <ol>
             {list.map((item) => (
-              <li key={item.id}> {item.listName}</li>
+              <li key={item.id}>
+                {" "}
+                {item.listName}{" "}
+                <button onClick={(event) => editHandler(item)}>Edit</button>
+              </li>
             ))}
           </ol>
         </div>
         <div className="absentlist">
           <h3> Absense Student Student</h3>
           <ul>
+            {/* <li> jsdklfjdsfdjf</li>
             <li> jsdklfjdsfdjf</li>
-            <li> jsdklfjdsfdjf</li>
-            <li> jsdklfjdsfdjf</li>
+            <li> jsdklfjdsfdjf</li> */}
           </ul>
         </div>
       </div>
